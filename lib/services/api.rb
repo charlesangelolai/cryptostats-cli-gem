460c890d35498c7a0c19e9ac1de208b53aa29a79
Charles <charlesangeloplai@gmail.com>
2020-12-01 15:56:19 -0500

    update api

--- lib/services/api.rb
+++ lib/services/api.rb
@@ -1,7 +1,9 @@
+# https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=dff57c31-f1ac-4e58-ac07-74bd976ede24
+
 class Api
 
     def self.base_url
-        "pro-api.coinmarketcap.com/v1/cryptocurrency"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     end
 
     def self.load_data
@@ -9,10 +11,10 @@ class Api
     end
 
     def self.load_cryptocurrency
-        response = RestClient.get 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY='
+        response = RestClient.get base_url
         data = JSON.parse(response.body)
 
-        data["data"].each do |crypto_data|
+        data.each do |crypto_data|
             CryptoStats.new(crypto_data)
         end
     end
