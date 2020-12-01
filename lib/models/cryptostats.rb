54a8d006424e4cded40599b2f66e4c5359a78a2e
Charles <charlesangeloplai@gmail.com>
2020-12-01 16:28:46 -0500

    update models

--- lib/models/cryptostats.rb
+++ lib/models/cryptostats.rb
@@ -14,16 +14,24 @@ class CryptoStats
     end
 
     def display_stats
-        puts "Image: #{self.image}"
+        puts "----------------------------------------------------------------------------------------------------"
+        #crypto_logo(self.image)
         puts "ID: #{self.id}"
-        puts "Symbol: #{self.symbol}"
-        puts "Current Price: #{self.current_price}"
-        puts "Market Cap: #{self.market_cap}"
-        puts "Total Volume: #{self.total_volume}"
-        puts "High (24hrs): #{self.high_24h}"
-        puts "Low (24hrs): #{self.low_24h}"
-        puts "Price Change (24hrs): #{self.price_change_24h}"
-        puts "Percentage Change (24hrs): #{self.price_change_percentage_24h}"
+        puts "Symbol: #{self.symbol.upcase}"
+        puts "Current Price: $#{self.current_price}"
+        puts "Market Cap: $#{self.market_cap}"
+        puts "Total Volume: $#{self.total_volume}"
+        puts "High (24hrs): #{self.high_24h}%"
+        puts "Low (24hrs): #{self.low_24h}%"
+        puts "Price Change (24hrs): $#{self.price_change_24h}"
+        puts "Percentage Change (24hrs): #{self.price_change_percentage_24h}%"
         puts "Last Updated: #{self.last_updated}"
+        puts "----------------------------------------------------------------------------------------------------"
     end
+
+    # def crypto_logo(image)
+    #     binding.pry
+    #     ascii = ASCII_Image.new(image)
+    #     ascii.build(20)
+    # end
 end
