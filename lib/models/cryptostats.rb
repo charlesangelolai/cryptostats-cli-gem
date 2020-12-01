a151919bae2ea9bfc7e03efe642563e891a8f735
Charles <charlesangeloplai@gmail.com>
2020-12-01 15:57:16 -0500

    update models

--- lib/models/cryptostats.rb
+++ lib/models/cryptostats.rb
@@ -12,4 +12,18 @@ class CryptoStats
     def self.all
         @@all.uniq
     end
+
+    def display_stats
+        puts "Image: #{self.image}"
+        puts "ID: #{self.id}"
+        puts "Symbol: #{self.symbol}"
+        puts "Current Price: #{self.current_price}"
+        puts "Market Cap: #{self.market_cap}"
+        puts "Total Volume: #{self.total_volume}"
+        puts "High (24hrs): #{self.high_24h}"
+        puts "Low (24hrs): #{self.low_24h}"
+        puts "Price Change (24hrs): #{self.price_change_24h}"
+        puts "Percentage Change (24hrs): #{self.price_change_percentage_24h}"
+        puts "Last Updated: #{self.last_updated}"
+    end
 end
