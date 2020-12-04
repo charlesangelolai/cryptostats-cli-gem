0269c8d5e469b83f205772dba12124c0b7931999
Charles <charlesangeloplai@gmail.com>
2020-12-04 14:36:10 -0500

    fix initialize method

--- lib/models/cryptostats.rb
+++ lib/models/cryptostats.rb
@@ -1,22 +1,29 @@
 class CryptoStats
+    attr_accessor :id, :symbol, :name, :image, :current_price, :market_cap, :market_cap_rank, :fully_diluted_valuation, :total_volume, 
+            :high_24h, :low_24h, :price_change_24h, :price_change_percentage_24h, :market_cap_change_24h, :market_cap_change_percentage_24h, 
+            :circulating_supply, :total_supply, :max_supply, :ath, :ath_change_percentage, :ath_date, :atl, :atl_change_percentage, :atl_date, :roi, :last_updated
+
     @@all = []
 
     def initialize(attributes)
-        attributes.each do |key, value| 
-            self.class.attr_accessor(key)
-            self.send(("#{key}="), value)
-            @@all << self
-        end
+        # attributes.each do |key, value|
+        #     self.class.attr_accessor(key)
+        #     self.send(("#{key}="), value)
+        #     @@all << self
+        # end
+
+        attributes.each {|key, value| self.send(("#{key}="), value)}
+        @@all << self
     end
 
     def self.all
-        @@all.uniq
+        @@all
     end
 
     def display_stats
         puts "-------------------------------------------- Crypto Stats --------------------------------------------"
         #crypto_logo(self.image)
-        puts "ID: #{self.id.capitalize}"
+        puts "Name: #{self.name}"
         puts "Symbol: #{self.symbol.upcase}"
         puts "Current Price: $#{self.current_price}"
         puts "Market Cap: $#{self.market_cap}"
